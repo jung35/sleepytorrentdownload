e7f05d2e25c18cf4c6d9d423820a6c30bb50f763
Jung Oh <Jung3o@yahoo.com>
2014-06-13 23:56:25 -0500

    working on serverside

--- src/downloadTorrent.js
+++ src/downloadTorrent.js
@@ -1,9 +1,7 @@
 var torrentStream = require('torrent-stream');
-var fs = require('fs');
-var Buffer = require('buffer').Buffer;
 var async = require('async');
 
-var lastPiece = null, currPiece = null, pieceCount = 0;
+var lastPiece = null, currPiece = null, pieceCount = -1;
 var numPiece = 0, highestPiece = 0;
 
 var currDownSize = 0;
@@ -15,19 +13,22 @@ var currUpSpeed = 0;
 var url = 'magnet:?xt=urn:btih:d89d08d448efa0827cce742ef9efae47076cee53&dn=Game+of+Thrones+Season+1%2C2%2C3+complete+&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
 
 var opt = {
-    connections: 100,     // Max amount of peers to be connected to.
-    tmp: 'C:/wamp/www/sleepytorrentdownload/src',          // Root folder for the files storage.
-    path: 'C:/wamp/www/sleepytorrentdownload/src/download', // Where to save the files. Overrides `tmp`.
+  tmp: 'C:/wamp/www/sleepytorrentdownload/src',
+  path: 'C:/wamp/www/sleepytorrentdownload/src/download',
 };
 
 var engine = torrentStream(url, opt);
 
 engine.on('ready', function() {
+  console.log('doiexist2');
   numPiece = engine.torrent.pieces.length;
 
   engine.files.forEach(function(file) {
+    console.log('file:', file.path);
     stream = file.createReadStream();
   });
+      engine.destroy();
+      process.exit();
 
   async.whilst(function () {
     return true;
@@ -64,6 +65,9 @@ engine.on('ready', function() {
 });
 
 engine.on('download', function(piece) {
+  if(pieceCount == -1) {
+    pieceCount = 0;
+  }
   currPiece = piece;
   pieceCount++;
 });
--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -37,13 +37,13 @@ function moreTorrents() {
   <div class="form-group"> \
     <label for="torrent-name-'+optionsCount+'" class="col-sm-2 control-label">File Name '+optionsCount+'</label> \
     <div class="col-sm-10"> \
-      <input name="torrent['+optionsCount+'][name]" type="text" class="form-control" id="torrent-name-'+optionsCount+'" placeholder="File Name"> \
+      <input type="text" class="form-control" id="torrent-name-'+optionsCount+'" placeholder="File Name"> \
     </div> \
   </div> \
   <div class="form-group"> \
     <label for="torrent-magnet-'+optionsCount+'" class="col-sm-2 control-label">Magnet Link '+optionsCount+'</label> \
     <div class="col-sm-10"> \
-      <input name="torrent['+optionsCount+'][magnet]" type="text" class="form-control" id="torrent-magnet-'+optionsCount+'" placeholder="Magnet Link"> \
+      <input type="text" class="form-control" id="torrent-magnet-'+optionsCount+'" placeholder="Magnet Link"> \
     </div> \
   </div> \
 </div>';
@@ -56,13 +56,13 @@ function clearTorrents() {
   <div class="form-group"> \
     <label for="torrent-name-'+optionsCount+'" class="col-sm-2 control-label">File Name '+optionsCount+'</label> \
     <div class="col-sm-10"> \
-      <input name="torrent['+optionsCount+'][name]" type="text" class="form-control" id="torrent-name-'+optionsCount+'" placeholder="File Name"> \
+      <input type="text" class="form-control" id="torrent-name-'+optionsCount+'" placeholder="File Name"> \
     </div> \
   </div> \
   <div class="form-group"> \
     <label for="torrent-magnet-'+optionsCount+'" class="col-sm-2 control-label">Magnet Link '+optionsCount+'</label> \
     <div class="col-sm-10"> \
-      <input name="torrent['+optionsCount+'][magnet]" type="text" class="form-control" id="torrent-magnet-'+optionsCount+'" placeholder="Magnet Link"> \
+      <input type="text" class="form-control" id="torrent-magnet-'+optionsCount+'" placeholder="Magnet Link"> \
     </div> \
   </div> \
 </div>';
@@ -94,3 +94,127 @@ function doDelete(id) {
       $('#alertBox').modal('show');
     });
 }
+
+function getSimpleByte(num) {
+  if(num > 1073741824) {
+    num = parseFloat(num / 1073741824.0).toFixed(2) + ' GB';
+  } else if(num > 1048576) {
+    num = parseFloat(num / 1048576.0).toFixed(2) + ' MB';
+  } else if(num > 1024) {
+    num = parseFloat(num / 1024.0).toFixed(2) + ' KB';
+  } else {
+    num += " B";
+  }
+
+  return num;
+}
+
+var socket = io();
+var $newTorrentForm = $('#addNewTorrent .torrent-add-form');
+$('form.torrent-add-form').submit(function() {
+  var optionsCount = $('.torrent-add').size();
+
+  var torrent = [];
+
+  for(var x = 1; x <= optionsCount; x++) {
+    var tName = $newTorrentForm.find('#torrent-name-'+x).val(),
+        tMagnet = $newTorrentForm.find('#torrent-magnet-'+x).val();
+
+    if(typeof tName == "string" &&
+       typeof tMagnet == "string" &&
+       tName.length > 0 &&
+       tMagnet.length > 0)
+    {
+      torrent.push({
+        name: tName,
+        magnet: tMagnet
+      });
+    }
+  }
+
+
+  socket.emit('newTorrent', torrent, function(err) {
+    if(err) {
+      var invTorrents = '';
+      for(var x = 0; x < err.length; x++) {
+        invTorrents += '<br>' + err[x];
+      }
+      $('#alertBox').find('.modal-body').html('<b>Invalid Magnet Link</b>' + invTorrents);
+      $('#alertBox').modal('show');
+    }
+  });
+  clearTorrents();
+  return false;
+});
+
+var $torrentList = $('.torrent-list'),
+    $torrentInfo = $('.torrent-info');
+socket.on('newTorrent', function(torrent) {
+  var torrentCount = $('.torrent-list li').size()+1;
+  var date = new Date(torrent.time);
+  var daynight = date.getHours() - 12 > 12 ? 'PM' : 'AM';
+
+  var sum = 0;
+  var fileNames = '';
+
+  for(var x = 0; x < torrent.files.length; x++) {
+    sum += torrent.files[x].size;
+    fileNames += '\r\n'+ torrent.files[x].path + '  --  '+getSimpleByte(torrent.files[x].size);
+  }
+
+  sum = getSimpleByte(sum);
+
+  var listFormat = '<li id="torrent-view-'+torrentCount+'"> \
+  <div class="row"> \
+    <div class="col-sm-12 torrent-list-name">#'+torrentCount+' '+torrent.name+'</div> \
+  </div> \
+  <div class="row text-muted"> \
+    <div class="col-sm-6 torrent-list-files">Files: '+torrent.files.length+'</div> \
+    <div class="col-sm-6 torrent-list-size">Estimated Size: '+sum+'</div> \
+  </div> \
+  <div class="row"> \
+    <div class="col-sm-12 torrent-list-progress"> \
+      <span class="text-muted">In Queue</span> \
+    </div> \
+  </div> \
+</li>';
+
+  var infoFormat = '<li id="torrent-info-'+torrentCount+'"> \
+  <h2>Super-duper-long-name-for-my-torrent <small>Added ' + date.getFullYear() + '/' + (date.getMonth()+1) + '/' + date.getDate() + ' '+(date.getHours() - (daynight == 'PM' ? 12:0))+':'+date.getMinutes()+' '+daynight+'</small></h2> \
+  <fieldset><legend>Files <small class="text-muted">'+torrent.files.length+'</small></legend> \
+    <pre>'+fileNames+'</pre> \
+  </fieldset> \
+  <fieldset><legend>Download <small class="text-muted">In Queue</small></legend> \
+    <dl class="col-xs-6 dl-horizontal text-monospaced"> \
+      <dt>Progress</dt> \
+      <dd>In Queue</dd> \
+    </dl> \
+    <dl class="col-xs-6 dl-horizontal text-monospaced"> \
+      <dt>Ratio</dt> \
+      <dd>&infin;</dd> \
+    </dl> \
+    <dl class="col-xs-6 dl-horizontal text-monospaced"> \
+      <dt>Downloaded</dt> \
+      <dd>&infin;</dd> \
+      <dt>Avg. Download Speed</dt> \
+      <dd>&infin;</dd> \
+    </dl> \
+    <dl class="col-xs-6 dl-horizontal text-monospaced"> \
+      <dt>Uploaded</dt> \
+      <dd>&infin;</dd> \
+      <dt>Avg. Upload Speed</dt> \
+      <dd>&infin;</dd> \
+    </dl> \
+    <dl class="col-xs-6 dl-horizontal text-monospaced"> \
+      <dt>Connections</dt> \
+      <dd>&infin;</dd> \
+    </dl> \
+  </fieldset> \
+  <fieldset><legend></legend> \
+    <button onClick="javascript:doDelete(4);" type="button" class="btn btn-danger btn-lg btn-block">Delete Torrent</button> \
+  </fieldset> \
+</li>';
+
+  $torrentList.append(listFormat);
+  $torrentInfo.append(infoFormat);
+});
